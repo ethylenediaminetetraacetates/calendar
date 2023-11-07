bcf9a08961fbc2138a8fe1125ea52068e27f13d9
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-11-06 21:49:50 -0500

    new themes and updated browsing mechanism
    lemon, forest, and sprout.
    the new wonderful browsing menu works

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-let themearr = ["charcoal.css", "midnight.css", "embers.css", "marble.css", "calcite.css", "tomato.css", "pumpkin.css", "highlighter.css"];
+let themearr = ["charcoal.css", "midnight.css", "embers.css", "marble.css", "calcite.css", "tomato.css", "pumpkin.css", "lemon.css", "highlighter.css", "sprout.css", "forest.css"];
 let themeind = 0;
 
-function togglemode(){
-    themeind++;
+function modeup(){
+    themeind = themeind + 1;
     themeind = themeind % themearr.length;
 
-    document.getElementById("themeindicator").innerHTML = "current theme: "+themearr[themeind].split(".css")[0]
+    document.getElementById("themeindicator").innerHTML = themearr[themeind].split(".css")[0];
+
+    document.querySelector("head > link:nth-child(1)").href = "themes/"+themearr[themeind];
+}
+function modedown(){
+    themeind = themeind - 1;
+    if(themeind < 0){themeind = themearr.length - 1}
+    themeind = themeind % themearr.length;
+
+    document.getElementById("themeindicator").innerHTML = themearr[themeind].split(".css")[0];
 
     document.querySelector("head > link:nth-child(1)").href = "themes/"+themearr[themeind];
 }
