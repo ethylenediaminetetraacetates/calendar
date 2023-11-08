d9ea8c4c6c3309abde9c3d7c1694408a55f83a5e
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-11-07 20:07:26 -0500

    added new features
    these were:
    a preview of the theme's colors
    allows you to change the day abbreviations

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
-let themearr = ["charcoal.css", "midnight.css", "embers.css", "marble.css", "calcite.css", "tomato.css", "pumpkin.css", "lemon.css", "highlighter.css", "sprout.css", "forest.css"];
+let themearr = ["charcoal.css", "midnight.css", "embers.css", "marble.css", "calcite.css", "tomato.css", "pumpkin.css", "canary.css", "lemon.css", "highlighter.css", "sprout.css", "forest.css"];
 let themeind = 0;
+
+
+
+let dayabbrs = [
+    ["S","M","T","W","T","F","S"],
+    ["Su","M","Tu","W","Th","F","Sa"],
+    ["Su","Mo","Tu","We","Th","Fr","Sa"],
+    ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+]
+
+let settings = {
+    dayabbr:1
+}
+
+function updsettings(){
+    for(let i = 0; i < 7; i++){
+        document.querySelector("body > div.fullcalendargrid > div.calendardaygrid > div:nth-child("+(i+1)+")").
+        innerHTML = dayabbrs[settings.dayabbr][i]
+    }
+}
+
+function daup(){
+    if(settings.dayabbr > dayabbrs.length - 1){
+        settings.dayabbr = 3;
+    }else{
+        settings.dayabbr = settings.dayabbr + 1;
+    }
+    document.getElementById("dayabbrindicator").innerHTML = 
+        dayabbrs[settings.dayabbr][1] + ", "+ 
+        dayabbrs[settings.dayabbr][2] + ", "+
+        dayabbrs[settings.dayabbr][3];
+    
+    updsettings();
+}
+
+function dadown(){
+    if(settings.dayabbr < 1){
+        settings.dayabbr = 0;
+    }else{
+        settings.dayabbr = settings.dayabbr - 1;
+    }
+    document.getElementById("dayabbrindicator").innerHTML = 
+        dayabbrs[settings.dayabbr][1] + ", "+ 
+        dayabbrs[settings.dayabbr][2] + ", "+
+        dayabbrs[settings.dayabbr][3];
+    
+    updsettings();
+}
 
 function modeup(){
     themeind = themeind + 1;
