18a47f15f1faa1a3e05f57020aabad38eff434a1
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-11-11 20:41:00 -0500

    added date-handling functions
    just for months, for now.

--- dates.js
+++ dates.js
@@ -1,2 +1,25 @@
 let calendar = {};
-calendar.currentDate = new Date();
+
+calendar.currentdate = new Date();
+calendar.selecteddate = new Date();
+
+calendar.month = {};
+calendar.month.days = 0;
+calendar.month.firstday = 0;
+
+calendar.refreshdaymonths = function(){
+
+    let date = calendar.selecteddate;
+
+    date.setMonth(date.getMonth()+1)
+    date.setDate(0)
+
+    calendar.month.days = date.getDate();
+
+    date.setDate(1)
+
+    calendar.month.firstday = date.getDay(); 
+
+}
+
+calendar.refreshdaymonths();
