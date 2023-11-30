55403c72712ddc076d1e9db6e9c4f9dabf380d2c
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-11-29 20:36:52 -0500

    updated notifstack feature frameworks.
    not ready yet

--- notifhandler.js
+++ notifhandler.js
@@ -2,15 +2,15 @@ let notif = {};
 
 notif.html = {};
 
-notif.html.default = ""; // expand later 
-
 notif.stack = [];
 notif.config = {};
 notif.config.showtime = 2000;
 
 notif.add = function(msg){
 
-    document.getElementById("notifstack").appendChild()
+    n = document.getElementById("notifstack").appendChild(document.createElement("div"));
+    n.classList.add("notification");
+    n.classList.add("notif"+notif.stack.length);
 
     notif.stack.push(
         {msg:msg, time: new Date()}
